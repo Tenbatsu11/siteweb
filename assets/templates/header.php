9a9e5c6ac8ab19061689562db2b8cc02a75ecc5d
Matrice <contact.mat.pineau@gmail.com>
2025-10-18 16:57:19 +0200

    Add header and kanjihome template files; update title in header

--- assets/templates/header.php
+++ assets/templates/header.php
@@ -4,7 +4,7 @@
  <head>
      <meta charset="UTF-8" />
      <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-     <title>Header</title>
+        <title>Kumiai</title>
      <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
      <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.11.3/font/bootstrap-icons.min.css">
      <link rel="stylesheet" href="/siteweb/assets/css/override-boostrap.css">
@@ -16,7 +16,7 @@
             </a> 
         </div>
      <ul class="nav col-12 col-md-auto mb-2 justify-content-center mb-md-0">
-         <li><a href="#" class="nav-link px-2 link-secondary">Home</a></li>
+         <li><a href="https://localhost/siteweb/index.php" class="nav-link px-2 link-secondary">Home</a></li>
          <li><a href="#" class="nav-link px-2">Kanjis</a></li>
          <li><a href="#" class="nav-link px-2">Vocabulaire</a></li>
          <li><a href="#" class="nav-link px-2">FAQs</a></li>
