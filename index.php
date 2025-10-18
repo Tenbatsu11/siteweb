f0997d33990fa409a604659a7fde30e14fce7833
Matrice <contact.mat.pineau@gmail.com>
2025-10-18 17:59:44 +0200

    Adding template pages before doing DB call and automatisation

--- index.php
+++ index.php
@@ -1,27 +1,31 @@
 <?php
 
-    require_once (__DIR__.'/assets/templates/header.php');
+require_once(__DIR__ . '/assets/templates/header.php');
 ?>
 
 <!DOCTYPE html>
-<html> 
-    <head>
-        <title>Accueil - Kumiai</title>
-    </head>
-    <body>
-        <main class="fade-in">
-            <div class ="container">
-                <a href="https://kumiai.sukai.moe/fr/">
-                    <img width="1028" src="/siteweb/assets/images/logo-kumiai.png" alt="Kumiai">
-                </a>
-            </div>        
-        <h1>Bienvenue sur Kumiai</h1>
-        <div class="mb-4">
+<html>
+
+<head>
+    <title>Accueil - Kumiai</title>
+</head>
+
+<body>
+    <main class="fade-in">
+        <div class="container">
+            <a href="https://kumiai.sukai.moe/fr/">
+                <img width="1028" src="/siteweb/assets/images/logo-kumiai.png" alt="Kumiai">
+            </a>
+        </div>
+        <div class="container">
+            <h1>Bienvenue sur Kumiai</h1>
+
             <p>Votre plateforme d'apprentissage du japonais.</p>
         </div>
-        </main>
-        <?php
-        require_once (__DIR__.'/../assets/templates/footer.php');   
+    </main>
+    <?php
+    require_once(__DIR__ . '/assets/templates/footer.php');
     ?>
-    </body>
+</body>
+
 </html>
