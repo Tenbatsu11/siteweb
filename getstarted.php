9940de9a8583609a21627c7ecdef9bb56cc59d2d
Matrice <contact.mat.pineau@gmail.com>
2025-12-12 20:51:22 +0100

    Add getstarted.php with guide for installing Japanese keyboard on Windows, Linux, and MacOS
    Add getstarted.php with guide for installing Japanese keyboard on Windows, Linux, and MacOS

---
+++ getstarted.php
@@ -0,0 +1,125 @@
+<?php
+
+require_once(__DIR__ . '/assets/templates/header.php');
+require_once(__DIR__ . '/libs/pdo.php');
+?>
+
+<!DOCTYPE html>
+<html>
+
+<head>
+    <meta charset="UTF-8">
+    <title>Kanji - Home</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.11.3/font/bootstrap-icons.min.css">
+    <link rel="stylesheet" href="/siteweb/assets/css/override-boostrap.css">
+</head>
+
+<body>
+    <main class="fade-in d-flex align-items-center justify-content-center flex-column">
+
+        <h1>Bien Commencer avec le Japonais : Installer le Clavier Japonais sur Windows</h1>
+        <h2>Pour pouvoir écrire en japonais sur ton ordinateur Windows,Linux ( ici Ubuntu ) ou encore MacOS.</h2>
+        <p><strong>Voici un guide étape par étape pour t'aider à le faire facilement.</strong></p>
+
+        <div class="accordion" id="accordionWindows">
+            <div class="accordion-item">
+                <h2 class="accordion-header">
+                    <button class="accordion-button" type="button" data-bs-toggle="collapse" data-bs-target="#collapseWindows" aria-expanded="true" aria-controls="collapseWindows">
+                        <strong>Installer le clavier japonais sous Windows, c'est super simple. Voilà ce qu'il faut faire :</strong>
+                    </button>
+                </h2>
+                <div id="collapseWindows" class="accordion-collapse collapse" data-bs-parent="#accordionWindows">
+                    <div class="accordion-body">
+                        <ol>
+                            <li>D'abord, tu cliques sur le bouton <strong>Démarrer</strong> en bas à gauche, comme d'habitude.</li>
+                            <li>Ensuite, tu vas dans <strong>Paramètres</strong> (l'icône en forme de roue dentée).</li>
+                            <li>Là, dans la fenêtre des paramètres, tu cliques sur <strong>Heure et langue</strong>.</li>
+                            <li>Ensuite, dans le menu de gauche, tu choisis <strong>Langue</strong>.</li>
+                            <li>Là, tu vois un bouton pour <strong>Ajouter une langue</strong>. Clique dessus et tape <strong>Japonais</strong> dans la barre de recherche.</li>
+                            <li>Tu sélectionnes <strong>Japonais</strong> (日本語) dans la liste et tu cliques sur <strong>Suivant</strong>, puis <strong>Installer</strong>.</li>
+                            <br>
+                            <p><strong>Une fois que la langue est installée, il faut maintenant ajouter le clavier japonais.</strong></p>
+                            <li>Tu retournes dans les paramètres de langue, tu vois Japonais dans la liste des langues installées, tu cliques dessus, puis sur <strong>Options</strong>.</li>
+                            <li>Là, tu cliques sur <strong>Ajouter un clavier</strong> et tu choisis <strong>Clavier japonais</strong>.</li>
+                            <br>
+                            <p><strong>Et voilà, maintenant il suffit de changer de langue quand tu veux écrire en japonais.</strong></p>
+                            <li>Dans la barre des tâches, près de l'horloge, tu vois une icône avec les lettres de la langue, clique dessus et choisis <strong>JP</strong> (Japonais).</li>
+                            <li>Maintenant tu peux écrire en hiragana, katakana, ou même en kanji ! Pour passer d'une langue à l'autre, il suffit d'appuyer sur <strong>Alt + Shift</strong>.</li>
+                            <br>
+
+                        </ol>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <br>
+        <div class="accordion" id="accordionLinux">
+            <div class="accordion-item">
+                <h2 class="accordion-header">
+                    <button class="accordion-button" type="button" data-bs-toggle="collapse" data-bs-target="#collapseLinux" aria-expanded="true" aria-controls="collapseLinux">
+                        <strong>Installer la disposition du clavier japonais sous Linux (Ubuntu) :</strong>
+                    </button>
+                </h2>
+                <div id="collapseLinux" class="accordion-collapse collapse" data-bs-parent="#accordionLinux">
+                    <div class="accordion-body">
+                        <ol>
+                            <li>D'abord, tu ouvres les <strong>Paramètres</strong> de ton ordinateur. C'est souvent dans le menu des applications, ou tu peux cliquer sur l'icône en haut à droite de ton écran.</li>
+                            <li>Dans les paramètres, tu choisis <strong>Région et langue</strong>.</li>
+                            <li>Ensuite, sous <strong>Langues</strong>, tu cliques sur <strong>Gérer les langues</strong>, puis tu cliques sur <strong>Ajouter une langue</strong>.</li>
+                            <li>Cherche <strong>Japonais</strong> (日本語) dans la liste et ajoute-le.</li>
+                            <br>
+                            <p><strong>Maintenant il faut ajouter le clavier japonais !</strong></p>
+                            <li>Dans <strong>Région et langue</strong>, sous <strong>Sources d'entrée</strong>, tu cliques sur <strong>Ajouter</strong>.</li>
+                            <li>Là, tu choisis <strong>Clavier japonais</strong> dans la liste et tu cliques sur <strong>Ajouter</strong>.</li>
+                            <br>
+                            <p><strong>Quand tu veux passer au clavier japonais, c’est facile :</strong></p>
+                            <li>Clique sur l’icône de la langue en haut à droite et sélectionne <strong>Japonais</strong>.</li>
+                            <li>Ou bien, tu peux utiliser le raccourci <strong>Super</strong> (la touche Windows)<strong> + Espace</strong> pour basculer entre les langues.</li>
+                            <br>
+                        </ol>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div class="accordion" id="accordionMacOS">
+            <div class="accordion-item">
+                <h2 class="accordion-header">
+                    <button class="accordion-button" type="button" data-bs-toggle="collapse" data-bs-target="#collapseMacOS" aria-expanded="true" aria-controls="collapseMacOS">
+                        <strong>Installer la disposition du clavier japonais sur MacOS :</strong>
+                    </button>
+                </h2>
+                <div id="collapseMacOS" class="accordion-collapse collapse" data-bs-parent="#accordionInstall">
+                    <div class="accordion-body">
+                        <ol>
+                            <li>D’abord, tu cliques sur le menu <strong>Pomme</strong> (en haut à gauche de l’écran) et tu choisis <strong>Préférences Système</strong>.</li>
+                            <li>Dans la fenêtre qui s’ouvre, clique sur <strong>Clavier</strong>.</li>
+                            <li>Ensuite, va dans l’onglet <strong>Méthodes de saisie</strong> et clique sur le bouton + en bas à gauche pour ajouter une nouvelle langue.</li>
+                            <li>Cherche <strong>Japonais</strong> et sélectionne-le. Tu peux choisir <strong>Kana</strong> ou <strong>Romaji</strong>, à toi de voir ce que tu préfères.</li>
+                            <li>Clique sur <strong>Ajouter</strong>.</li>
+                            <br>
+                            <p><strong>Maintenant, pour passer au clavier japonais, c'est encore plus facile.</strong></p>
+                            <li>Une fois que tu as ajouté le japonais, tu vois une petite icône en haut à droite (près de l'horloge) pour changer de langue.</li>
+                            <li>Clique dessus et choisis <strong>Japonais</strong> pour commencer à écrire en japonais.</li>
+                            <br>
+                        </ol>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div class="section d-flex align-items-center justify-content-center flex-column">
+            <h2><strong>Quelques petits trucs à savoir :</strong></h2>
+    <ul>
+        <li><strong>Hiragana et Katakana :</strong> Quand tu écris en japonais, tu peux facilement passer entre le <strong>Hiragana</strong> et le <strong>Katakana </strong> appuyant sur <strong>Caps Lock</strong>. Et si tu veux écrire en kanji, tu tapes les mots en romaji et tu appuies sur <strong>Espace</strong> pour voir les suggestions de kanji.</li>
+        <li><strong>Changer rapidement de langue :</strong> Si tu utilises plusieurs langues sur ton ordinateur, tu peux configurer un raccourci clavier pour passer d'une langue à l'autre rapidement. Par exemple,<strong> Alt + Maj</strong> ou <strong>Ctrl + Espace.</strong></li>
+        <li><strong>Pratique avec les raccourcis :</strong> Tous les systèmes modernes permettent de personnaliser les raccourcis pour changer de clavier ou de langue, ce qui te permet d'écrire plus vite et de manière plus fluide.</li>
+    </ul>
+    <br>
+    <p>Voilà, c'est tout ! Avec ça, tu devrais être prêt à écrire en japonais sur ton PC ou ton Mac, peu importe le système. Si tu as un doute ou si tu veux des précisions, n'hésite pas à demander, je serai là pour t'aider !</p>
+    </main>
+    <?php
+    require_once(__DIR__ . '/assets/templates/footer.php');
+    ?>
+</body>
+
+</html>
