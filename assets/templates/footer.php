bc2a140535e5bf6b5e8c16aaec1e202ada73ab8e
Matrice <contact.mat.pineau@gmail.com>
2025-12-17 11:54:12 +0100

    Add footer links and create legal page with legal information

--- assets/templates/footer.php
+++ assets/templates/footer.php
@@ -1,9 +1,9 @@
 
 <footer class="py-3 my-4">
     <ul class="nav justify-content-center border-bottom pb-3 mb-3">
-        <li class="nav-item"><a href="#" class="nav-link px-2 text-body-secondary">Accueil</a></li>
-        <li class="nav-item"><a href="#" class="nav-link px-2 text-body-secondary">Annonces</a></li>
-        <li class="nav-item"><a href="#" class="nav-link px-2 text-body-secondary">Mentions légales</a></li>
+        <li class="nav-item"><a href="https://localhost/siteweb/index.php" class="nav-link px-2 text-body-secondary">Accueil</a></li>
+        <li class="nav-item"><a href="https://localhost/siteweb/getstarted.php" class="nav-link px-2 text-body-secondary">Bien Commencer</a></li>
+        <li class="nav-item"><a href="https://localhost/siteweb/legal.php" class="nav-link px-2 text-body-secondary">Mentions légales</a></li>
     </ul>
     <p class="text-center text-body-secondary">©Kumiai</p>
 </footer>
